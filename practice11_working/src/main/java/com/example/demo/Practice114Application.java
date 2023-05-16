f484bec4755bbe891173eedfeeee01fbd9ad0a33
Arsenii01 <100406298+Arsenii01@users.noreply.github.com>
2023-05-16 18:58:01 +0300

    Add files via upload

---
+++ practice11_working/src/main/java/com/example/demo/Practice114Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Practice114Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Practice114Application.class, args);
+	}
+
+}
